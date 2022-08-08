b5081301a9612e3a5e0245fe56c15eff633d3cff
Boris Novosad <novosad.b@mail.ru>
2022-08-08 19:53:49 +0300

    correction Parser_A1 and Parser_Jokes

--- Parser_A1.py
+++ Parser_A1.py
@@ -1,18 +1,32 @@
-# import requests
-# from bs4 import BeautifulSoup
-# url = 'https://www.a1.by/ru/search?text=%D1%82%D0%B5%D0%BB%D0%B5%D1%84%D0%BE%D0%BD%D1%8B'
-# href = 1
-# while True:
-#     if href != 1:
-#         url = 'https://www.a1.by/' + href
-#     request = requests.get(url)
-#     soup = BeautifulSoup(request.text, "html.parser")
-#     teme = soup.find_all('div', class_="product-search-item-title")
-#     mobile = []
-#     for i in teme:
-#         print(i.text)
-#     try:
-#         href = (soup.find('a', class_='link link--primary pagination-link icon icon--arrow-right')).get('href')
-#     except AttributeError:
-#         print('Товар закончился')
-#         break
+import requests
+from bs4 import BeautifulSoup
+url = 'https://www.a1.by/ru/search?text=%D1%82%D0%B5%D0%BB%D0%B5%D1%84%D0%BE%D0%BD%D1%8B'
+href = 1
+count = 1
+mobile = []
+min_prise_list = []
+max_prise_list = []
+while True:
+    if href != 1:
+        url = 'https://www.a1.by/' + href
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, "html.parser")
+    teme_phone = soup.find_all('div', class_="product-search-item-title")
+    for i in teme_phone:
+        mobile.append(i.text)
+    teme_min_prise = soup.find_all('span', class_="price-value")
+    print(f'Страница №{count}\n---------------------')
+    for i in teme_min_prise:
+        min_prise_list.append(i.text.replace(u'\xa0', ''))
+    try:
+        href = (soup.find('a', class_='link link--primary pagination-link icon icon--arrow-right')).get('href')
+    except AttributeError:
+        print('Товар закончился')
+        break
+    count += 1
+
+count = 0
+for i in range(len(mobile)):
+    print(f'Название телефона: {mobile[i]}\nЦена от {min_prise_list[count]} руб./мес - 24 мес'
+          f'\nот {min_prise_list[count + 1]}')
+    count += 2
--- Parser_Jokes.py
+++ Parser_Jokes.py
@@ -2,16 +2,19 @@
 # from bs4 import BeautifulSoup
 # url = 'https://nekdo.ru/short/'
 # href = 1
+# count = 1
 # while True:
 #     if href != 1:
 #         url = 'https://nekdo.ru' + href
 #     request = requests.get(url)
 #     soup = BeautifulSoup(request.text, "html.parser")
 #     teme = soup.find_all('div', class_="text")
+#     print(f'Страница № {count}\n-------------------------')
 #     for i in teme:
-#         print(i.text,'\n')
+#         print(i.text, end='\n-------------------------\n')
 #     try:
 #         href = (soup.find('a', class_='sel')).get('href')
 #     except AttributeError:
-#         print('Товар закончился')
+#         print('Анекдоты все')
 #         break
+#     count += 1
