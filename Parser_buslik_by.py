abcb4674942b5642caf3b69520d360e4375439b1
Boris Novosad <novosad.b@mail.ru>
2022-08-14 23:43:44 +0300

    Buslik

--- Parser_buslik_by.py
+++ Parser_buslik_by.py
@@ -44,35 +44,35 @@ import requests
 #         print(product_page_parsing(i.get('href')))
 #     page += 1
 
-# url = 'https://buslik.by/catalog/podguzniki/podguzniki_1/'
-# page = 2
-# price_list = []
-# title_list = []
-# item_discount_list = []
-# while True:
-#     if page != 2:
-#         url = 'https://buslik.by' + href
-#     r = requests.get(url)
-#     r.encoding = 'UTF-8'
-#     soup = BeautifulSoup(r.text, 'html.parser')
-#     teme_catalog_item__prices = soup.find_all('div', class_='catalog-item__prices')
-#     for i in teme_catalog_item__prices:
-#         price_list.append((i.get_text(strip=True)).split(' руб.'))
-#     teme_title = soup.find_all('a', class_='catalog-item__name')
-#     for i in teme_title:
-#         title_list.append(i.get_text(strip=True))
-#     href = soup.find_all('a', class_='new-pagination__link')
-#     for i in href:
-#         if i.text == str(page):
-#             href = i.get('href')
-#             break
-#     else:
-#         break
-#     page += 1
-# result = dict(zip(title_list, price_list))
-# id = 1
-# for k, v in result.items():
-#     print(f'{id}. Наименование: {k}\nЦена: {v[0]} руб.' if v[1] == ''
-#           else f'{id}. Наименование: {k}\nЦена: {v[0]} руб. Старая цена {v[1]} руб. Скидка: '
-#                f'{floor(((float(v[1]) - float(v[0])) / float(v[1])) * 100)} %')
-#     id += 1
+url = 'https://buslik.by/catalog/podguzniki/podguzniki_1/'
+page = 2
+price_list = []
+title_list = []
+item_discount_list = []
+while True:
+    if page != 2:
+        url = 'https://buslik.by' + href
+    r = requests.get(url)
+    r.encoding = 'UTF-8'
+    soup = BeautifulSoup(r.text, 'html.parser')
+    teme_catalog_item__prices = soup.find_all('div', class_='catalog-item__prices')
+    for i in teme_catalog_item__prices:
+        price_list.append((i.get_text(strip=True)).split(' руб.'))
+    teme_title = soup.find_all('a', class_='catalog-item__name')
+    for i in teme_title:
+        title_list.append(i.get_text(strip=True))
+    href = soup.find_all('a', class_='new-pagination__link')
+    for i in href:
+        if i.text == str(page):
+            href = i.get('href')
+            break
+    else:
+        break
+    page += 1
+result = dict(zip(title_list, price_list))
+id = 1
+for k, v in result.items():
+    print(f'{id}. Наименование: {k}\nЦена: {v[0]} руб.' if v[1] == ''
+          else f'{id}. Наименование: {k}\nЦена: {v[0]} руб. Старая цена {v[1]} руб. Скидка: '
+               f'{floor(((float(v[1]) - float(v[0])) / float(v[1])) * 100)} %')
+    id += 1
