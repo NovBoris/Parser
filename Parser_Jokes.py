d23740d33b9a5eab3e49647675cab78222303476
Boris Novosad <novosad.b@mail.ru>
2022-08-19 23:21:58 +0300

    Jokes

--- Parser_Jokes.py
+++ Parser_Jokes.py
@@ -1,20 +1,20 @@
-# import requests
-# from bs4 import BeautifulSoup
-# url = 'https://nekdo.ru/short/'
-# href = 1
-# count = 1
-# while True:
-#     if href != 1:
-#         url = 'https://nekdo.ru' + href
-#     request = requests.get(url)
-#     soup = BeautifulSoup(request.text, "html.parser")
-#     teme = soup.find_all('div', class_="text")
-#     print(f'Страница № {count}\n-------------------------')
-#     for i in teme:
-#         print(i.text, end='\n-------------------------\n')
-#     try:
-#         href = (soup.find('a', class_='sel')).get('href')
-#     except AttributeError:
-#         print('Анекдоты все')
-#         break
-#     count += 1
+import requests
+from bs4 import BeautifulSoup
+url = 'https://nekdo.ru/short/'
+href = 1
+count = 1
+while True:
+    if href != 1:
+        url = 'https://nekdo.ru' + href
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, "html.parser")
+    teme = soup.find_all('div', class_="text")
+    print(f'Страница № {count}\n-------------------------')
+    for i in teme:
+        print(i.text, end='\n-------------------------\n')
+    try:
+        href = (soup.find('a', class_='sel')).get('href')
+    except AttributeError:
+        print('Анекдоты все')
+        break
+    count += 1
